545e2488638b24634d69ffe19817287203a5de08
rajatkumarco <rajat.kumar@catalystone.com>
2020-02-05 13:05:46 +0530

    just committing

---
+++ src/main/java/SpringBoot/Project/DemoController.java
@@ -0,0 +1,16 @@
+package SpringBoot.Project;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DemoController {
+
+	@RequestMapping("/welcome")
+	public String welcome(Map<String, Object> model) {
+		return "welcome";
+	}
+	
+}
